5327221f3e00a5416454daaab0070311a931a6cc
nodiary <rohilkie94@gmail.com>
2024-01-10 23:07:36 +0900

    2강 - 이미지 넣는 법, public 폴더 이용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import bg from './img/bg.jpeg';
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home"></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#sale">Sale</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            {/* pulbic 폴더 이미지 사용 권장 방식 */}
+            {/* <img src={process.env.PUBLIC_URL + '/logo192.png'} */}
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
