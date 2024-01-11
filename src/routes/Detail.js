e67a0c0c485cf13c2c665e2b068e6b8458af32a2
nodiary <rohilkie94@gmail.com>
2024-01-11 15:22:59 +0900

    styled-components

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,32 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+// styled-components 라이브러리 사용.
+// 스타일이 다른 js파일로 오염되지 않는다.
+// 페이지 로딩 시간 단축.
+// 오염방지하려면 컴포넌트.module.css로 파일명 변경 시 해당 js파일에만 종속되게 할 수있음
+let YellowBtn = styled.button`
+    background : ${props => props.bg};
+    color : ${props => props.bg == 'blue' ? 'white' : 'black'};
+    padding : 10px;
+`
+// 기존 스타일 복사 가능
+let NewBtn = styled.button(YellowBtn)`
+
+`
+let Box = styled.div`
+    background : grey;
+    padding : 20px;
+`
 
 function Detail(props) {
     const {id} = useParams();
     const item = props.shoes.find((item) => item.id == id);
+
     return(
         <div className="container">
+             <YellowBtn bg="blue">버톤</YellowBtn>
+             <YellowBtn bg="yellow">버톤</YellowBtn>
             <div className="row">
                 <div className="col-md-6">
                 <img src={"https://codingapple1.github.io/shop/shoes"+(item.id+1)+".jpg"} width="100%" />
