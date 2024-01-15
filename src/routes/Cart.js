c0c5c6062580c517de44211a87dccde51d2b23d3
nodiary <rohilkie94@gmail.com>
2024-01-15 11:09:58 +0900

    Redux 3  - store의 state 변경하기

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,13 +1,14 @@
 import { Table } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { changeName, addCount, subsCount } from "./../store.js";
 function Cart() {
+  let user = useSelector((states) => states.user)
+  let userCartList = useSelector((states) => states.userCartList);
 
-    // let states = useSelector((states) => { return states })
-    // let user = useSelector((states) => states.user)
-    // let stock = useSelector((states )=> states.stock)
-    let userCartList = useSelector((states) => states.userCartList)
+  let dispatch = useDispatch();
   return (
     <>
+      {user}의 장바구니
       <Table>
         <thead>
           <tr>
@@ -18,19 +19,21 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          {
-            userCartList.map((item)=>{
-                return (
-                    <tr key={item.id}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.count}</td>
-                        <td>안녕</td>
-                    </tr>
-                )   
-            })
-          }
-          
+          {userCartList.map((item, i) => 
+              <tr key={i}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.count}</td>   
+                <td>
+                    <button onClick={()=>{
+                        dispatch(addCount(item.id))
+                    }}>+</button>
+                    <button onClick={()=>{
+                        dispatch(subsCount(item.id))
+                    }}>-</button>
+                </td>
+              </tr>
+          )}
         </tbody>
       </Table>
     </>
--- src/store.js
+++ src/store.js
@@ -2,8 +2,15 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 let user = createSlice({
   name : 'user',
-  initialState : 'kim'
+  initialState : 'kim',
+  reducers : {
+    changeName(state){
+      return 'john' + state;
+    }
+  }
 })
+
+export let { changeName } = user.actions
 
 let stock = createSlice({
   name : 'stock',
@@ -13,16 +20,34 @@ let stock = createSlice({
 let userCartList = createSlice({
   name : 'userCartList',
   initialState : 
-  [
-    {id : 0, name : 'White and Black', count : 2},
-    {id : 2, name : 'Grey Yordan', count : 1}
-  ] 
+    [
+      {id : 0, name : 'White and Black', count : 2},
+      {id : 2, name : 'Grey Yordan', count : 1}
+    ] ,
+  reducers : {
+    addCount(state, action){
+      const { payload: id } = action;
+      return state.map(item =>
+        item.id === id ? { ...item, count: item.count + 1 } : item
+      );
+    } ,
+    
+    subsCount(state, action){
+      const { payload: id } = action;
+      return state.map(item =>
+        item.id === id ? { ...item, count: Math.max(item.count - 1, 0) } : item
+      );
+    }
+  }
 })
+
+export let { addCount, subsCount } = userCartList.actions
 
 export default configureStore({
   reducer: { 
     user : user.reducer,
     stock : stock.reducer,
     userCartList : userCartList.reducer
+
   }
 }) 
